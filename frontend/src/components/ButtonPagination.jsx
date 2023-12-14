037c0bdaa4845190fc79f60ed089668acb788646
ryugenxd <anonimous7770@gmail.com>
2023-12-14 11:20:12 +0800

    add pagination

--- frontend/src/components/ButtonPagination.jsx
+++ frontend/src/components/ButtonPagination.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import AxiosClient from '../client/AxiosClient';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const ButtonPagination = ({item}) => {
-  const getData = ()=>{
-    console.log(item);
+  const {setContacs} = useStateContext();
+  const getDataContact = () => {
+    AxiosClient.get(`/${item.url.split('api/')[1]}`)
+    .then(({data})=>{
+      setContacs(data);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   }
   return (
     <button
-    onClick={getData}
+    onClick={getDataContact}
     className={`p-2 ${item.active?'bg-green-600':'bg-slate-900'} text-lg whitespace-nowrap`}
     dangerouslySetInnerHTML={{ __html:item.label}}>
     </button>
